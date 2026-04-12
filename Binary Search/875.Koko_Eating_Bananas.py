7d6d3b8e4bf261d2ac6df42be437ac1b39a320ad
5amp12 <samuel.passey@hotmail.com>
2026-04-12 12:42:31 +0100

    completed problem 875. Koko Eating Bananas

---
+++ Binary Search/875.Koko_Eating_Bananas.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        smallestSpeed = r
+        while l <= r:
+            mid = (l + r) // 2
+            time = 0
+            for num in piles:
+                time += math.ceil(num/mid)
+            if time <= h:
+                if mid < smallestSpeed:
+                    smallestSpeed = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return smallestSpeed
