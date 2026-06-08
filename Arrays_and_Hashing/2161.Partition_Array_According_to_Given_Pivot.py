653bba95f4be1ce504f719f8b74cb0e2e58d42e2
5amp12 <samuel.passey@hotmail.com>
2026-06-08 22:54:02 +0100

    completed: 2161. Partition Array According to Given Pivot

---
+++ Arrays_and_Hashing/2161.Partition_Array_According_to_Given_Pivot.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        lPiv = []
+        piv = []
+        rPiv = []
+        for i in range(len(nums)):
+            print(nums[i])
+            if nums[i] > pivot:
+                rPiv.append(nums[i])
+            elif nums[i] < pivot:
+                lPiv.append(nums[i])
+            else nums[i] == pivot:
+                piv.append(nums[i])
+        
+        return lPiv + piv + rPiv
+
